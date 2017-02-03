d0605c9a6dd6e78fcd653e8e113ca79c3d1f31ba
FengAiLing2016 <learn_python>
2017-02-03 11:13:40 +0800

    v2.0

--- user/user.py
+++ user/user.py
@@ -1,32 +1,37 @@
-num = 0
+num = 1
 temp = 1
-# 循环输入
-while num < 3:
-    user = input('User: ')
-    f = open('/learn_python/learn_python/learn_python/user/user_lock.txt', 'r')
+# 判断用户名是否被锁定
+while num:
+    user = input('User: ')  # 输入用户名
+    f = open('/learn_python/learn_python/user/user_lock.txt', 'r')  # 打开锁定用户列表
+    # 判断
     for line in f.readlines():
-        if user == line:
+        if user == line.strip('\n'):
             print('lock!')
-            temp = 0
-            num=4
+            num = 0
+            temp = 1
             break
+        else:
+            num = 0
+            temp = 2
     f.close()
-    if temp:
-        f = open('/learn_python/learn_python/learn_python/user/user.txt', 'r')
-        for line in f.readlines():
-            user_save, pass_save = line.strip('\n').split()
-            if user == user_save:
-                pass_word = input('Password: ')
-                if pass_word == pass_save:
+# 判断密码输入是否正确
+if temp == 2:
+    f = open('/learn_python/learn_python/user/user.txt', 'r')  # 打开用户列表
+    for line in f.readlines():
+        user_save, pass_save = line.strip('\n').split()
+        if user == user_save:  # 判断用户名
+            while num < 3:  # 重试3次
+                pass_word = input('Password: ')  # 输入密码
+                if pass_word == pass_save:  # 判断密码正确
                     print('good!')
-                    num = 6
-                    break
+                    num = 5
                 else:
-                    num += 1
-            elif user != user_save:
-                print('not user')
-                break
-        f.close()
+                    print('password is error')  # 提示密码错误
+                    num += 1  # 重试次数
+            break  # 如果用户名正确跳出循环不再进行剩余的用户名判断
+    f.close()
+# 当重试次数=3次将用户名写入锁定列表
 if num == 3:
-    with open('/learn_python/learn_python/learn_python/user/user_lock.txt', 'a') as f:
-        f.write(user)
+    with open('/learn_python/learn_python/user/user_lock.txt', 'a') as f:
+        f.write('\n' + user)
